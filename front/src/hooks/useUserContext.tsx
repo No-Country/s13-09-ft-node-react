fe43fc517443a52db3959321c8ef7b5c85f9a99a
Gonzalo Suarez <gonzasuarez100@gmail.com>
2024-02-14 17:49:38 -0300

    next inicializado

--- front/src/hooks/useUserContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { UserContext } from "@/context/userContext";
-
-export const useUserContext = () => {
-	const context = React.useContext(UserContext);
-	if (!context) throw new Error("useUserContext debe estar dentro del proveedor UserContext");
-	return context;
-};
--- front/src/services/newsletter.ts
+++
@@ -1,7 +0,0 @@
-import { NewsletterPayload, NewsletterResponse } from "@/types/api";
-import api from "./api";
-
-export const suscribeNewsletter = async (payload: NewsletterPayload) => {
-	const response = await api.post<NewsletterResponse>("/newsletter", payload);
-	return response.data;
-};
