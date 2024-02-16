2670cfe67001c2ae87b4b589ef11cb8300b2a410
Arturo Guerra <arturoalejandroguerram@gmail.com>
2024-02-15 19:23:10 -0500

    auth routes/controller/services/midd

--- back/routes/index.routes.js
+++ back/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import authRoutes from "./auth.route";
 
 const router = Router();
 
 router.use("/", (req, res) => {
   res.send("hola");
 });
+router.use("/auth", authRoutes);
 
 export default router;
