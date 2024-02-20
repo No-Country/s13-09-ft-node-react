23f07b9ca9112ec5618f56f7df299317e012b148
Bruno Ken <bruno_am_22@hotmail.com>
2024-02-20 18:49:05 -0300

    complete create-turn

---
+++ front/src/app/components/mapbox/index.jsx
@@ -0,0 +1,39 @@
+import Map,{Marker, GeolocateControl,FullscreenControl,NavigationControl,ScaleControl,Popup} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+
+export function Mapbox({ ubication,direction}) {
+  const coordinates = ubication;
+
+  return (
+    <>
+          <Map
+              mapLib={import('mapbox-gl')}
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX}
+            initialViewState={{
+                longitude:ubication[1],
+                latitude: ubication[0],
+                zoom:14.5
+              }}
+              
+            style={{width: "100%", height: 500}}
+            mapStyle="mapbox://styles/mapbox/streets-v12"
+          >
+                <Marker longitude={coordinates[1]} latitude={coordinates[0]}></Marker>
+                <FullscreenControl position="top-left" />
+                <NavigationControl position="top-left" />
+                <ScaleControl />
+                <GeolocateControl />
+                <Popup
+                    anchor="top"
+                    longitude={coordinates[1]}
+                    latitude={coordinates[0]}
+                >
+                    <div>
+                        {direction}
+                    </div>
+                </Popup>
+          </Map>
+    </>
+  );
+}
