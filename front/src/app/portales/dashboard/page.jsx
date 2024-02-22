3546504334db97562bc7466b4cd58aee54f67f00
Rolando Duarte <rolandoduarte83@gmail.com>
2024-02-22 15:46:39 -0300

    creacion dashboardPage

--- front/src/app/portales/dashboard/page.jsx
+++ front/src/app/portales/dashboard/page.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import DoctorSidebar from '../components/DoctorSidebar';
-import PatientSidebar from '../components/PatientSidebar';
+import React from "react";
+import Image from "next/image";
+import DoctorSidebar from "../components/DoctorSidebar";
+import PatientSidebar from "../components/PatientSidebar";
+import SearchBar from "../../ui/SearchBar";
+import Clock from "../../ui/Clock";
+import Notifications from "@/app/ui/svg/Notifications ";
+
 
 const DashboardPage = ({ userType }) => {
     return (
-        <div className="flex">
-            {/* Sidebar según el tipo de usuario */}
-            {/* {userType === 'medico' ? <DoctorSidebar /> : <PatientSidebar />} */}
-            {userType === 'medico' ? <PatientSidebar /> : <DoctorSidebar />}
-
+        <div className="flex h-screen flex-col">
             {/* Contenido principal */}
-            <div className="flex-1 p-4">
-                <h1 className="text-2xl font-semibold mb-4">Dashboard</h1>
-                {/* Aquí puedes agregar el contenido específico del dashboard */}
+            <div className="flex flex-1">
+                {/* Sidebar según el tipo de usuario */}
+                {userType === "medico" ? <PatientSidebar /> : <DoctorSidebar />}
+
+                {/* Contenido del Dashboard */}
+                <div className="flex-1 p-4">
+                    {/* Contenido superior */}
+                    <div className="m-4 flex justify-between items-center">
+                        {/* Aquí colocamos el SearchBar */}
+                        <SearchBar />
+
+                        {/* Aquí está el AnalogClock */}
+                        <Clock />
+
+                        <Notifications />
+
+                        {/* Div que contiene la imagen del doctor y su nombre */}
+                        <div className="flex items-center">
+                            <Image
+                                width={60}
+                                height={60}
+                                src="/assets/doc.jpg"
+                                alt="doc img"
+                                className="mr-4 h-8 w-8 rounded-full"
+                            />
+                            <span className="text-lg font-semibold text-gray-900">
+                                Dr. Ottoneli
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
---
+++ front/src/app/ui/svg/Options.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Options = () => {
+    return (
+        <svg
+            width="48"
+            height="48"
+            viewBox="0 0 48 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g clipPath="url(#clip0_487_1162)">
+                <rect width="48" height="48" rx="6" fill="#F8F8F8" />
+                <rect
+                    x="13.5"
+                    y="16.498"
+                    width="21"
+                    height="1"
+                    rx="0.5"
+                    fill="#121212"
+                    stroke="#121212"
+                />
+                <rect
+                    x="13.5"
+                    y="23.4971"
+                    width="21"
+                    height="1"
+                    rx="0.5"
+                    fill="#121212"
+                    stroke="#121212"
+                />
+                <rect
+                    x="13.5"
+                    y="31.4956"
+                    width="21"
+                    height="1"
+                    rx="0.5"
+                    fill="#121212"
+                    stroke="#121212"
+                />
+                <circle
+                    cx="19"
+                    cy="16.9985"
+                    r="2.25"
+                    fill="#F8F8F8"
+                    stroke="#121212"
+                    strokeWidth="1.5"
+                />
+                <circle
+                    cx="29"
+                    cy="23.9976"
+                    r="2.25"
+                    fill="#F8F8F8"
+                    stroke="#121212"
+                    strokeWidth="1.5"
+                />
+                <circle
+                    cx="19"
+                    cy="31.9961"
+                    r="2.25"
+                    fill="#F8F8F8"
+                    stroke="#121212"
+                    strokeWidth="1.5"
+                />
+            </g>
+            <defs>
+                <clipPath id="clip0_487_1162">
+                    <rect width="48" height="48" fill="white" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
+
+export default Options;
