790271ac42109cf11ae55f3eb794147e6cb348a5
Sonny <spimentelm1201@gmail.com>
2024-02-26 16:01:56 -0500

    fix ssl connection postgres

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -9,43 +9,44 @@ import { readdirSync } from "fs";
 
 // import path  here
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
 const db = {};
 
 const sequelize = new Sequelize(
-  `${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
+    `${process.env.DB_DIALECT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?ssl=true`, {
+        logging: false,
+        native: false,
+    }
 );
 
 const modelsDir = path.resolve(__dirname);
 
 const files = readdirSync(modelsDir).filter(
-  (file) =>
+    (file) =>
     file.indexOf(".") !== 0 &&
     file !== basename(__filename) &&
     file.slice(-3) === ".js"
 );
 
 for await (const file of files) {
-  // use path here to access your models from models directory then await for it @
-  const modelPath = path.resolve(modelsDir, file);
-  const model = await import(`file://${modelPath}`);
-  if (model.default) {
-    const namedModel = await model.default(sequelize, DataTypes);
-    db[namedModel.name] = namedModel;
-  }
+    // use path here to access your models from models directory then await for it @
+    const modelPath = path.resolve(modelsDir, file);
+    const model = await
+    import (`file://${modelPath}`);
+    if (model.default) {
+        const namedModel = await model.default(sequelize, DataTypes);
+        db[namedModel.name] = namedModel;
+    }
 }
 
 Object.keys(db).forEach((modelName) => {
-  if (modelName) {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
+    if (modelName) {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
     }
-  }
 });
 
 db.sequelize = sequelize;
