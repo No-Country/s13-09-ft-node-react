dd84533538350681fdbaa969c895cc13588bbf42
Arturo Guerra <arturoalejandroguerram@gmail.com>
2024-02-26 10:38:37 -0500

    changes in auth midd

--- back/src/middleware/authVerificationMiddleware.js
+++ back/src/middleware/authVerificationMiddleware.js
@@ -1,23 +1,25 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-import HttpResponse from "../helpers/HttpResponse";
+import HttpResponse from "../helpers/HttpResponse.js";
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
-function authVerificationMiddleware(req, res, next) {
-  try {
-    const token = req.headers["Authorization"];
+function authVerificationMiddleware(roles){
+  return (req, res, next) => {
+    try {
+      const token = req.headers["authorization"];
 
-    if (!token) HttpResponse.unauthorized(res, {message: "Token not provided"});
+      if (!token) return HttpResponse.unauthorized(res, {message: "Token not provided"});
   
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        return HttpResponse.unauthorized(res);
-      };
-      next();
-    });
-  } catch (error) {
-    return HttpResponse.serverError(res);
+      jwt.verify(token, SECRET_KEY, (err, {user}) => {
+        if (err || !roles.includes(user.role)) {
+          return HttpResponse.unauthorized(res);
+        };
+        next();
+      });
+    } catch (error) {
+      return HttpResponse.serverError(res);
+    };
   };
 };
 
