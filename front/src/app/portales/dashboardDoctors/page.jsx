b158c99e0bda88a1f323fbacf60393802f0b54fb
Rolando Duarte <rolandoduarte83@gmail.com>
2024-03-01 12:48:57 -0300

    arreglando class por className en dshboardDoctors

--- front/src/app/portales/dashboardDoctors/page.jsx
+++ front/src/app/portales/dashboardDoctors/page.jsx
@@ -55,19 +55,6 @@ const DashboardPage = ({ userType }) => {
                         </div>
 
 
-                        <div className="flex items-center">
-                            <Image
-                                width={60}
-                                height={60}
-                                src="/assets/doctor.png"
-                                alt="doc img"
-                                className="mr-4 h-8 w-8 rounded-full"
-                            />
-                            <span className="text-lg font-semibold text-gray-900">
-                                Dr. Ottoneli
-                            </span>
-                        </div>
-
                     </div>
 
                     {/* Tarjeta de tareas asignadas */}
@@ -87,7 +74,6 @@ const DashboardPage = ({ userType }) => {
                                         <td className="flex border bg-white rounded border-gray-300 p-2 items-center">
 
                                             <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
-                                            <div class="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
                                             <div>
                                                 <p className="font-semibold text-black"> Rondas de pacientes  </p>
                                                 <p> 25 de enero de 2024 | 9:00 </p>
@@ -98,7 +84,7 @@ const DashboardPage = ({ userType }) => {
                                     <tr className="m-2">
                                         <td className="flex border bg-white rounded border-gray-300 p-2 items-center">
                                             <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
-                                            <div class="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
+
                                             <div>
                                                 <p className="font-semibold text-black"> Revision de resultados de laboratorio </p>
                                                 <p> 25 de enero de 2024 | 15:00 </p>
@@ -108,7 +94,6 @@ const DashboardPage = ({ userType }) => {
                                     <tr className="m-2">
                                         <td className="flex border bg-white rounded border-gray-300 p-2 items-center">
                                             <div className="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
-                                            <div class="w-10 h-10 flex justify-center items-center rounded-full bg-gradient-to-br from-blue-700 to-blue-500 text-white text-3xl font-bold mr-4">P</div>
                                             <div>
                                                 <p className="font-semibold text-black"> Procedimientos quirurgicos </p>
                                                 <p> 25 de enero de 2024 | 16:00 </p>
